062b6695ecd06119a053ff3f82f927f68b02b8dc
Oscar Vera <vera_reyes-10@hotmail.com>
2018-06-28 21:28:26 -0500

    Created Service Contract Layer

---
+++ SCL/IService.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using System.Collections.Generic;
+namespace SCL
+{
+    public interface IService
+    {
+        Product CreateProduct(Product newProduct);
+        Product GetProductByID(int productID);
+        bool UpdateProduct(Product productToUpdate);
+        bool DeleteProduct(int productID);
+        List<Product> GetProductsByCategoryID(int categoryID);
+
+        Category CreateCategory(Category newCategory);
+        Category GetCategoryByID(int categoryID);
+        bool UpdateCategory(Category categoryToUpdate);
+        bool DeleteCategory(int categoryID);
+        List<Category> GetCategories();
+    }
+}
