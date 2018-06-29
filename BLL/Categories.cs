185e3a4c8380ebfe6b58dba464002e33f903d6bc
Oscar Vera <vera_reyes-10@hotmail.com>
2018-06-28 21:19:26 -0500

    Created Business Logic layer

---
+++ BLL/Categories.cs
@@ -0,0 +1,89 @@
+﻿using DAL;
+using Entities;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class Categories
+    {
+        public Category Create(Category newCategory)
+        {
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                newCategory = repository.Create(newCategory);
+            }
+
+            return newCategory;
+        }
+
+        public Category GetByID(int categoryID)
+        {
+            Category result = null;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                result = repository.Get<Category>(p => p.CategoryID == categoryID);
+            }
+
+            return result;
+        }
+
+        public bool Update(Category categoryToUpdate)
+        {
+            bool result = false;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                Category tmpCategory = repository.Get<Category>(
+                    p => p.CategoryName == categoryToUpdate.CategoryName
+                    && p.CategoryID != categoryToUpdate.CategoryID);
+
+                if (tmpCategory == null)
+                    result = repository.Update(categoryToUpdate);
+                //else
+                // Here is anything that indicates category to update already exists
+            }
+
+            return result;
+        }
+
+        public bool Delete(int categoryID)
+        {
+            bool result = false;
+
+            Category category = GetByID(categoryID);
+            if (category != null)
+            {
+                if (category.Products.Count == 0)
+                {
+                    using (var repository = RepositoryFactory.CreateRepository())
+                    {
+                        result = repository.Delete(category);
+                    }
+                }
+                else
+                {
+                    // Here is anything that indicates category cannot be deleted
+                }
+            }
+            else
+            {
+                // Here is anything that indicates category does not exist
+            }
+
+            return result;
+        }
+
+        public List<Category> GetCategories()
+        {
+            List<Category> result = null;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                result = repository.GetAll<Category>(p => p.CategoryID >= 0);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ BLL/Products.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL;
+using Entities;
+
+namespace BLL
+{
+    public class Products
+    {
+        public Product Create(Product newProduct)
+        {
+            Product result = null;
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                Product res = repository.Get<Product>(p => p.ProductName == newProduct.ProductName);
+                if (res == null)
+                    result = repository.Create(newProduct);
+                //else
+                    // Here is anything that indicates product already exists
+            }
+
+            return result;
+        }
+
+        public Product GetByID(int productID)
+        {
+            Product result = null;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                result = repository.Get<Product>(p => p.ProductID == productID);
+            }
+
+            return result;
+        }
+
+        public bool Update(Product productToUpdate)
+        {
+            bool result = false;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                Product tmpProduct = repository.Get<Product>(
+                    p => p.ProductName == productToUpdate.ProductName
+                    && p.ProductID != productToUpdate.ProductID);
+
+                if (tmpProduct == null)
+                    result = repository.Update(productToUpdate);
+                //else
+                // Here is anything that indicates product to update already exists
+            }
+
+            return result;
+        }
+
+        public bool Delete(int productID)
+        {
+            bool result = false;
+
+            Product product = GetByID(productID);
+            if(product != null)
+            {
+                if (product.UnitsInStock == 0)
+                {
+                    using (var repository = RepositoryFactory.CreateRepository())
+                    {
+                        result = repository.Delete(product);
+                    }
+                }
+                else
+                {
+                    // Here is anything that indicates product cannot be deleted
+                }
+            }
+            else
+            {
+                // Here is anything that indicates product does not exist
+            }
+
+            return result;
+        }
+
+        public List<Product> FilterByCategoryID(int categoryID)
+        {
+            List<Product> result = null;
+
+            using (var repository = RepositoryFactory.CreateRepository())
+            {
+                result = repository.GetAll<Product>(p => p.CategoryID == categoryID);
+            }
+
+            return result;
+        }
+    }
+}
